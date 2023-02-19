ecf64857c37ecc3e7fdf8475556dbdf604b328b0
LemonSauc3 <riley@boyder.ca>
2023-02-19 12:00:11 -0500

    Minor Changes to test production build

--- app/page.js
+++ app/page.js
@@ -9,9 +9,14 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   const [count, setCount] = useState(0);
   return (
-    <main className="flex justify-center content-center">
+    <main className="flex justify-center content-center p-5">
       <div className="flex flex-col space-y-5">
         <h1 className="text-6xl content-center">Hello World</h1>
+        <div className="divider">Counter</div>
+        <h2 className=" flex justify-center content-center text-6xl text-bold">
+          {count}
+        </h2>
+
         <button
           className="btn"
           onClick={(e) => {
@@ -26,9 +31,6 @@ export default function Home() {
         <button className="btn" onClick={(e) => setCount(0)}>
           RESET
         </button>
-        <h2 className=" flex justify-center content-center text-2xl">
-          {count}
-        </h2>
       </div>
     </main>
   );
