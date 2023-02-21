6cccdc448fd01f2192917aab611ed79a5ff3fdec
LemonSauc3 <riley@boyder.ca>
2023-02-20 19:03:10 -0500

    Added a navbar and a footer with working links

--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
 "use client";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
 import { useState } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [count, setCount] = useState(0);
